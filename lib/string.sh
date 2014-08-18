cd293775bc699058690b8e9e81f91f5addf47eda
Niklas Rosenqvist <niklas.s.rosenqvist@gmail.com>
2014-08-18 16:46:55 +0200

    Fixed substring and added a preliminary cmp_version

--- lib/string.sh
+++ lib/string.sh
@@ -30,7 +30,7 @@ function string_ireplace_all {
 
 function substring {
     if [ $# -eq 2 ]; then
-        echo "${1:$2:$((${#1}-$2))}"
+        echo "${1:$2}"
     else
         echo "${1:$2:$3}"
     fi
@@ -46,4 +46,35 @@ function to_lowercase {
 
 function capitalize {
     echo "${1^}"
+}
+
+function cmp_version {
+    if [[ $1 == $2 ]]; then
+        return 0
+    fi
+
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            return 2
+        fi
+    done
+    return 0
 }
