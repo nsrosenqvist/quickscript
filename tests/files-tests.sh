8993d90a1dec0992ba3a73eea593ec6ef60ecb1f
Niklas Rosenqvist <niklas.s.rosenqvist@gmail.com>
2014-08-19 15:01:50 +0200

    Removed redundant messages

--- tests/files-tests.sh
+++ tests/files-tests.sh
@@ -3,15 +3,15 @@
 oldpwd="$(pwd)" && cd "$(dirname "$0")" && source ../quickscript.sh
 
 function testFileExtension() {
-	assertEquals "Wrong file extension returned" "txt" "$(file_extension "very-long.file.name.txt")"
+	assertEquals "txt" "$(file_extension "very-long.file.name.txt")"
 }
 
 function testFileName() {
-	assertEquals "Wrong file name returned" "very-long.file.name" "$(file_name "very-long.file.name.txt")"
+	assertEquals "very-long.file.name" "$(file_name "very-long.file.name.txt")"
 }
 
 function testStripMultiSlash() {
-	assertEquals "Slashes didn't get removed correctly" "/home/user/Documents/" "$(strip_multi_slash "//home////user/Documents//////")"
+	assertEquals "/home/user/Documents/" "$(strip_multi_slash "//home////user/Documents//////")"
 }
 
 function testIsMountPoint() {
