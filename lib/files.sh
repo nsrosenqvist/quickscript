762246fda8b62d15270ef887c7af52bfb8e28e3d
Niklas Rosenqvist <niklas.s.rosenqvist@gmail.com>
2014-08-19 14:56:10 +0200

    merged mount.sh into files.sh

--- lib/files.sh
+++ lib/files.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
-function file_extension {
+function file_extension() {
     local filename="$(basename "$1")"
     echo "${filename##*.}"
 }
 
-function file_name {
+function file_name() {
     local filename="$(basename "$1")"
     echo "${filename%.*}"
 }
 
-function strip_multi_slash {
+function strip_multi_slash() {
 	echo "$1" | sed 's#//*#/#g'
+}
+
+function is_mountpoint() {
+    mountpoint -q "$1"
+    is_mounted=$?
+
+    if [ $is_mounted -eq 0 ]; then
+        # Make sure provided file exists
+        if [ $# -ge 2 ]; then
+            if [ ! -e "$1/$2" ]; then
+                is_mounted=1
+            fi
+        fi
+    fi
+
+    echo $is_mounted
+    return $is_mounted
 }
